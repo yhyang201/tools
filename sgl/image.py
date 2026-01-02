17138b1796a003fb7d7a7611834e4249c0692393
Yuhao Yang <47235274+yhyang201@users.noreply.github.com>
2026-01-02 22:57:33 +0800

    Update image.py

---
+++ sgl/image.py
@@ -0,0 +1,27 @@
+import requests
+
+port = 30000
+
+url = f"http://localhost:{port}/v1/chat/completions"
+
+data = {
+    "model": "Qwen/Qwen2.5-VL-7B-Instruct",
+    "messages": [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "What’s in this image?"},
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": "https://github.com/sgl-project/sglang/blob/main/examples/assets/example_image.png?raw=true"
+                    },
+                },
+            ],
+        }
+    ],
+    "max_tokens": 300,
+}
+
+response = requests.post(url, json=data)
+print(response.text)
